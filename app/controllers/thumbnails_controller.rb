c5d9d50a1875e19d15a88bd6850c49d83371f62e
Bartek Nowakowski <bartek@bnowakowski.pl>
2016-12-20 00:22:45 +0100

    Succesfully get url of first flickr generated jpg.

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -7,15 +7,15 @@ class ThumbnailsController < ApplicationController
     Flickrie.shared_secret = ""
 
     set_id = 72157674133494003
-    photos = Flickrie.photos_from_set(set_id) # => [#<Photo: id="8232348", ...>, #<Photo: id="8194318", ...>, ...]
+    photos = Flickrie.get_photos_from_set(set_id) # => [#<Photo: id="8232348", ...>, #<Photo: id="8194318", ...>, ...]
 
     photo = photos.first
     p photo.id          # => "8232348"
-    p photo.url         # => "http://www.flickr.com/photos/67313352@N04/8232348"
-    p photo.title       # => "Samantha and me"
-    p photo.owner       # => #<User: nsid="67313352@N04", ...>
-    p photo.owner.nsid  # => "67313352@N04"
-    p 'debug'
+    # TODO figure out {o-secret} from https://www.flickr.com/services/api/misc.urls.html to generate oryginal photo url not flickr generated one
+    photo_url_prefix = 'https://farm' + photo.farm.to_s + '.staticflickr.com/' + photo.server.to_s + '/' + photo.id.to_s + '_' + photo.secret
+    photo_size = 'b'
+    photo_url_suffix =  '_' + photo_size + '.jpg'
+    p photo_url_prefix + photo_url_suffix
 
   end
 end
