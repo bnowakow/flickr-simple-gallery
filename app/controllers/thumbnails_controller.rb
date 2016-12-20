ff9370052bc48bf473e8d0f2e94109e656a48906
Bartek Nowakowski <bartek@bnowakowski.pl>
2016-12-20 01:58:34 +0100

    lost 20 pounds in 5 minutes! lel, removed code that was done by gem anyway (again)

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -6,25 +6,7 @@ class ThumbnailsController < ApplicationController
     Flickrie.api_key = ""
     Flickrie.shared_secret = ""
 
-    Flickrie.timeout = 100
-
     set_id = 72157676252232272
-    flickr_photos = Flickrie.get_photos_from_set(set_id, :include_sizes => true) # => [#<Photo: id="8232348", ...>, #<Photo: id="8194318", ...>, ...]
-
-    flickr_photos.each do |flickr_photo|
-      p flickr_photo.id
-      photo_url_prefix = 'https://farm' + flickr_photo.farm.to_s + '.staticflickr.com/' + flickr_photo.server.to_s + '/' + flickr_photo.id.to_s + '_'
-      flickr_phto_info = flickr_photo.get_info
-      (@photos ||= []).push({
-                                # thumbnail_url
-                                'url_z'         => flickr_photo['url_z'],
-                                # photo_url
-                                'url_l'         => flickr_photo['url_l'],
-                                # oryginal_size_url
-                                'url_orig'      => photo_url_prefix + flickr_phto_info['originalsecret'] + '_o.' + flickr_phto_info['originalformat']
-                                # 'url_o'         => flickr_photo['url_o'],
-                            })
-    end
-
+    @photos = Flickrie.get_photos_from_set(set_id, :include_sizes => true) # => [#<Photo: id="8232348", ...>, #<Photo: id="8194318", ...>, ...]
   end
 end
