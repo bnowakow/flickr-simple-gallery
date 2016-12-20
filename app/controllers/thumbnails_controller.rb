2e2fb431eddc913462a95e0ae62da48b3aa7ef6e
Bartek Nowakowski <bartek@bnowakowski.pl>
2016-12-20 01:56:28 +0100

    oryginal size image (I've just found that I did again same manual work that gem already does, watch out my next commit)

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -6,22 +6,25 @@ class ThumbnailsController < ApplicationController
     Flickrie.api_key = ""
     Flickrie.shared_secret = ""
 
-    set_id = 72157674133494003
+    Flickrie.timeout = 100
+
+    set_id = 72157676252232272
     flickr_photos = Flickrie.get_photos_from_set(set_id, :include_sizes => true) # => [#<Photo: id="8232348", ...>, #<Photo: id="8194318", ...>, ...]
 
-    # flickr_photos.each do |flickr_photo|
-    #   # TODO figure out {o-secret} from https://www.flickr.com/services/api/misc.urls.html to generate oryginal photo url not flickr generated one
-    #   photo_url_prefix = 'https://farm' + flickr_photo.farm.to_s + '.staticflickr.com/' + flickr_photo.server.to_s + '/' + flickr_photo.id.to_s + '_' + flickr_photo.secret + '_'
-    #   photo_url_suffix =  '.jpg'
-    #   p photo_url_prefix + photo_url_suffix
-    #   (@photos ||= []).push({
-    #                             'thumbnail_url' => flickr_photo['url_z'],
-    #                             'photo_url' => flickr_photo['url_l']
-    #                         })
-    # end
-
-    @photos = flickr_photos
-
+    flickr_photos.each do |flickr_photo|
+      p flickr_photo.id
+      photo_url_prefix = 'https://farm' + flickr_photo.farm.to_s + '.staticflickr.com/' + flickr_photo.server.to_s + '/' + flickr_photo.id.to_s + '_'
+      flickr_phto_info = flickr_photo.get_info
+      (@photos ||= []).push({
+                                # thumbnail_url
+                                'url_z'         => flickr_photo['url_z'],
+                                # photo_url
+                                'url_l'         => flickr_photo['url_l'],
+                                # oryginal_size_url
+                                'url_orig'      => photo_url_prefix + flickr_phto_info['originalsecret'] + '_o.' + flickr_phto_info['originalformat']
+                                # 'url_o'         => flickr_photo['url_o'],
+                            })
+    end
 
   end
 end
